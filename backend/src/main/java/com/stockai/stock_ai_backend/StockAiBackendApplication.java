3be5215cdd68a3a195ceac4a025a717596dec7cd
taha <tahaelkhayyam1@gmail.com>
2026-05-16 03:14:42 +0100

    Initial project setup: frontend + backend + ai service

---
+++ backend/src/main/java/com/stockai/stock_ai_backend/StockAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockai.stock_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stockai/stock_ai_backend/StockAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockai.stock_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
